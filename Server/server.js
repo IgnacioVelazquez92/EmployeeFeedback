df86fe97c2117c6bdde17d87d94098efea6cfe70
Ignacio Velazquez <ignaciov92@gmail.com>
2024-02-24 20:48:59 -0300

    se agrega otro script

--- Server/server.js
+++ Server/server.js
@@ -16,6 +16,4 @@ app.use(express.json());
 //Routes
 app.use("/employees", employeesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+module.exports = app;
